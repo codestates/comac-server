3d07c73dc4c6b6895cc8845bcf6ca4421d1bfcdb
lhb7021 <manevil6@gmail.com>
2021-06-09 19:04:22 +0900

    edit: index.js

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const https = require('https');
+const fs = require('fs');
+
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.json())
 app.get('/', (req, res) => {
   res.json('Hello World');
 })
-
-const server = app.listen(port, () => {
-  console.log(`http server listening on ${port}`);
-});
-
+let server;
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  server = https
+    .createServer(
+      {
+        key: fs.readFileSync(__dirname + `/` + 'key.pem', 'utf-8'),
+        cert: fs.readFileSync(__dirname + `/` + 'cert.pem', 'utf-8'),
+      },
+      app
+    )
+    .listen(port, () => {
+      console.log(`https server listening on ${port}`);
+    });
+} else {
+  server = app.listen(port, () => {
+    console.log(`http server listening on ${port}`);
+  });
+};
 module.exports = server;
