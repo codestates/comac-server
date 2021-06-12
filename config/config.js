d65ed3d941d72583977c05c68341178378a923f4
lhb7021 <manevil6@gmail.com>
2021-06-12 22:15:10 +0900

    edit: sequelize

--- config/config.js
+++ config/config.js
@@ -1,25 +1,54 @@
 require("dotenv").config();
 
+// RDS환경 mysql
 module.exports = {
   "development": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
+    "port": process.env.DATABASE_PORT,
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
+    "port": process.env.DATABASE_PORT,
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
+    "port": process.env.DATABASE_PORT,
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   }
 }
+
+// local환경 mysql
+// module.exports = {
+//   "development": {
+//     "username": process.env.DATABASE_USERNAME,
+//     "password": process.env.DATABASE_PASSWORD,
+//     "database": process.env.DATABASE_NAME,
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": process.env.DATABASE_USERNAME,
+//     "password": process.env.DATABASE_PASSWORD,
+//     "database": process.env.DATABASE_NAME,
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": process.env.DATABASE_USERNAME,
+//     "password": process.env.DATABASE_PASSWORD,
+//     "database": process.env.DATABASE_NAME,
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
